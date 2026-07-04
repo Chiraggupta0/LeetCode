2f19b15f6532c3841619885de38146f039ca7377
CHIRAG GUPTA <118209429+Chiraggupta0@users.noreply.github.com>
2026-07-05 00:07:27 +0530

    Time: 1 ms (100%), Space: 119.3 MB (14.54%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+        int n = nums.length;
+        int max = 0;
+        int best = nums[0];
+        for(int j=k;j<n;j++)
+            {
+                best = Math.max(best,nums[j-k]);
+                max = Math.max(max,best+nums[j]);
+            }
+        return max;
+    }
+}
