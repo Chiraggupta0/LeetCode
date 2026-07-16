4e43c73f6b6787951ee3bd3ec3237c30c6c1f8f0
CHIRAG GUPTA <118209429+Chiraggupta0@users.noreply.github.com>
2026-07-17 00:51:24 +0530

    Time: 2 ms (17.41%), Space: 46.4 MB (49.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,29 +1,49 @@
 class Solution {
-    int profit = 0;
-    public int rec(int[] prices,int idx,int flag,int[][] dp)
-    {
-        if(idx == prices.length) return 0;
-        if(dp[idx][flag] != -1) return dp[idx][flag];
-        if(flag == 1)
-        {
-            int take = 0-prices[idx]+rec(prices,idx+1,0,dp);
-            int nonTake = rec(prices,idx+1,1,dp);
-            dp[idx][flag] = Math.max(take,nonTake);
-        }
-        else
-        {
-            int t = prices[idx]+rec(prices,idx+1,1,dp);
-            int nt = rec(prices,idx+1,0,dp);
-            dp[idx][flag] = Math.max(t,nt);
-        }
-        return dp[idx][flag];
-    }
+    // int profit = 0;
+    // public int rec(int[] prices,int idx,int flag,int[][] dp)
+    // {
+    //     if(idx == prices.length) return 0;
+    //     if(dp[idx][flag] != -1) return dp[idx][flag];
+    //     if(flag == 1)
+    //     {
+    //         int take = 0-prices[idx]+rec(prices,idx+1,0,dp);
+    //         int nonTake = rec(prices,idx+1,1,dp);
+    //         dp[idx][flag] = Math.max(take,nonTake);
+    //     }
+    //     else
+    //     {
+    //         int t = prices[idx]+rec(prices,idx+1,1,dp);
+    //         int nt = rec(prices,idx+1,0,dp);
+    //         dp[idx][flag] = Math.max(t,nt);
+    //     }
+    //     return dp[idx][flag];
+    // }
     public int maxProfit(int[] prices) {
-        int[][]dp = new int[prices.length][2];
-        for(int i=0;i<prices.length;i++)
+        int[][]dp = new int[prices.length+1][2];
+        // for(int i=0;i<prices.length;i++)
+        // {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        dp[prices.length][1] = 0;
+        dp[prices.length][0] = 0;
+        for(int idx=prices.length-1;idx>=0;idx--)
         {
-            Arrays.fill(dp[i],-1);
+            for(int flag=1;flag>=0;flag--)
+            {
+                if(flag == 1)
+                {
+                    int take = 0-prices[idx]+dp[idx+1][0];
+                    int nonTake = dp[idx+1][1];
+                    dp[idx][flag] = Math.max(take,nonTake);
+                }
+                else
+                {
+                    int t = prices[idx]+dp[idx][1];
+                    int nt = dp[idx+1][0];
+                    dp[idx][flag] = Math.max(t,nt);
+                }
+            }
         }
-        return rec(prices,0,1,dp);
+        return dp[0][1];
     }
 }
