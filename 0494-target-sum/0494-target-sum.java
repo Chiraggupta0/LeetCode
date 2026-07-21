76060dd4b0e8cb7b9621e60bc144322b97a75d9e
CHIRAG GUPTA <118209429+Chiraggupta0@users.noreply.github.com>
2026-07-21 16:39:20 +0530

    Time: 7 ms (65.57%), Space: 46.9 MB (19.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,27 @@
 class Solution {
-    public static int rec(int[]nums,int target,int idx,int sum)
-    {
-        if(idx == nums.length)
-        {
-            if(sum == target)
-            {
-                return 1;
-            }
-            else return 0;
+
+    public int rec(int[] nums, int target, int idx, int sum, int[][] dp, int offset) {
+        if (idx == nums.length) {
+            return sum == target ? 1 : 0;
         }
-        int neg = rec(nums,target,idx+1,sum-nums[idx]);
-        int pos = rec(nums,target,idx+1,sum+nums[idx]);
-        return neg+pos;
+        if (dp[idx][sum + offset] != -1) {
+            return dp[idx][sum + offset];
+        }
+        int pos = rec(nums, target, idx + 1, sum + nums[idx], dp, offset);
+        int neg = rec(nums, target, idx + 1, sum - nums[idx], dp, offset);
+        return dp[idx][sum + offset] = pos + neg;
     }
+
     public int findTargetSumWays(int[] nums, int target) {
-        return rec(nums,target,0,0);
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        if (Math.abs(target) > total) return 0;
+        int[][] dp = new int[nums.length][2 * total + 1];
+        for (int i = 0; i < nums.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return rec(nums, target, 0, 0, dp, total);
     }
 }
