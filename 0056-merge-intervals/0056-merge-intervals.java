ec299fb2d8f5d87f889b1bd71e79059df105eb5a
CHIRAG GUPTA <118209429+Chiraggupta0@users.noreply.github.com>
2026-07-31 23:26:12 +0530

    Time: 8 ms (91.49%), Space: 49.3 MB (27.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> ans = new ArrayList<>();
         int n = intervals.length;
         for (int i = 0; i < n; i++) {
             int start = intervals[i][0];
             int end = intervals[i][1];
-            if (!ans.isEmpty()&& end<=ans.get(ans.size()-1)[1]) {
-                continue;
+            if (ans.isEmpty() || start > ans.get(ans.size() - 1)[1]) {
+                ans.add(new int[]{start, end});
+            } else {
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], end);
             }
-            for (int j=i+1;j<n;j++) {
-                if(intervals[j][0]<=end) {
-                    end = Math.max(end,intervals[j][1]);
-                } 
-                else{
-                    break;
-                }
-            }
-            ans.add(new int[]{start, end});
         }
         int[][] result = new int[ans.size()][];
-        for (int i = 0;i<ans.size();i++) {
+        for (int i = 0; i < ans.size(); i++) {
             result[i] = ans.get(i);
         }
         return result;
